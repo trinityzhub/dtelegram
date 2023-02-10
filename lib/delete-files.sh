156304599ee3fba10bffbd63bc8f40050f7d9133
joelz-jobs <79366529+joelz-jobs@users.noreply.github.com>
2023-02-10 10:10:10 +0000

    maxdepth fix and xargs run only stout

--- lib/delete-files.sh
+++ lib/delete-files.sh
@@ -1,9 +1,9 @@
 
 HOST_NAME="H3" 
 
-find $HOME  -name ".$HOST_NAME*" -maxdepth 1 -type d ! -path  "/home/ubbe/.h3" | xargs rm -r 
-find   $HOME/.local/share/applications/ -name "*$HOST_NAME*" | xargs rm -rf 
-find   $HOME/.local/bin/ -name  "*$HOST_NAME*" | xargs rm -rf 
+find $HOME  -maxdepth 1 -name ".$HOST_NAME*"  -type d ! -path  "/home/ubbe/.h3" | xargs -r rm -rf 
+find   $HOME/.local/share/applications/ -name "*$HOST_NAME*" | xargs -r rm -rf 
+find   $HOME/.local/bin/ -name  "*$HOST_NAME*" | xargs -r rm -rf 
 
 
 
