0a1276189037e654ad60e599addd8a20ed14cdfd
joelz-jobs <79366529+joelz-jobs@users.noreply.github.com>
2023-02-10 10:07:44 +0000

    delete by HOST NAME

--- lib/delete-files.sh
+++ lib/delete-files.sh
@@ -1,4 +1,20 @@
 
+HOST_NAME="H3" 
+
+find $HOME  -name ".$HOST_NAME*" -maxdepth 1 -type d ! -path  "/home/ubbe/.h3" | xargs rm -r 
+find   $HOME/.local/share/applications/ -name "*$HOST_NAME*" | xargs rm -rf 
+find   $HOME/.local/bin/ -name  "*$HOST_NAME*" | xargs rm -rf 
 
 
-find $HOME  -name ".h3*" -maxdepth 1 -type d
+
+# find   $HOME/.local/share/applications/ -name "Tele*" | xargs rm -rf 
+# find   $HOME/.local/bin/ -name "Tele*" | xargs rm -rf 
+
+
+
+
+
+docker ps -a | grep 'Telegram' | grep "$HOST_NAME" | xargs -r docker rm  
+
+
+
